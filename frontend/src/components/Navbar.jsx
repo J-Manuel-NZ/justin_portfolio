989a1bbde3738de0644a77e273af878bfea5c691
Justin Manuel <112037970+J-Manuel-NZ@users.noreply.github.com>
2023-05-25 17:18:52 +1200

    Added Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
 import WorkWithMeButton from './WorkWithMeButton'
+import './componentStyles.css'
+import { useState } from 'react'
+import CloseIcon from '../../public/close.svg'
+import MenuIcon from '../../public/menu.svg'
 
 const Navbar = ({title}) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
-    <div>
-        <div>
-            <h1>{title}</h1>
+    <div className='navbar'>
+        <div className='navbar-title'>
+            <a href='/'><h1>{title}</h1></a>
         </div>
 
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/interests">Interests</a></li>
+        <ul className='navbar-links hidden'>
+            <li className='inactive-page'><a href="/">home</a></li>
+            <li className='inactive-page'><a href="/portfolio">portfolio</a></li>
+            <li className='active-page'><a href="/bio">bio</a></li>
+            <li className='inactive-page'><a href="/interests">interests</a></li>
             <WorkWithMeButton />
         </ul>
+
+        <div>
+        {isMenuOpen 
+        ? <img className="cursor-pointer my-9 absolute z-20 right-6" src={CloseIcon} alt='Close Icon' onClick={() => setIsMenuOpen(false)} /> 
+        : <img className="cursor-pointer my-9 mx-6" src={MenuIcon} alt='Menu Icon' onClick={() => setIsMenuOpen(true)}/>}
+        {isMenuOpen && (
+          <ul className='navbar-menu'>
+            <li className='inactive-page'><a href="/">home</a></li>
+            <li className='inactive-page'><a href="/portfolio">portfolio</a></li>
+            <li className='active-page'><a href="/bio">bio</a></li>
+            <li className='inactive-page'><a href="/interests">interests</a></li>
+            <WorkWithMeButton />
+          </ul>
+        )}
+        </div>
+        
+
+
     </div>
   )
 }
